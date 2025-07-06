a8b92689200ecbdb16ad6d9be99e60f836d400c1
Bruno Ndiba Mbwaye Roy <brunombw@seas.upenn.edu>
2025-07-06 19:40:34 -0400

    first commit

---
+++ SmartPath/SmartPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPathApp.swift
+//  SmartPath
+//
+//  Created by Bruno Ndiba Mbwaye Roy on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartPathTests/SmartPathTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPathTests.swift
+//  SmartPathTests
+//
+//  Created by Bruno Ndiba Mbwaye Roy on 7/6/25.
+//
+
+import Testing
+@testable import SmartPath
+
+struct SmartPathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
